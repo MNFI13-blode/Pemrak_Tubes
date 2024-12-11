d74d3a099990a3fc822190bbbc37a84f33288dbf
Hermawan Saputra <126874286+hermwnS@users.noreply.github.com>
2024-12-11 23:21:52 +0700

    memodifikasi halaman deskripsi produk

--- telu_market/lib/deskripsi_produk.dart
+++ telu_market/lib/deskripsi_produk.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class DeskripsiProduk extends StatelessWidget{
@@ -7,19 +9,63 @@ class DeskripsiProduk extends StatelessWidget{
       appBar: AppBar(
         title: Text('Deskripsi Produk'),
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('HQ Mechanic Pencil 2B'),
-            Text('Pensil memiliki ketebalan yang pas untuk menulis serta menggambar'),
+            Image.asset(
+              'assets/pensil mekanik.jpeg',
+              width: double.infinity,
+              height: 150,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Toko Sukapura',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+
+                  Text(
+                    'HQ Pencil Mechanic 2B',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+
+                  Text(
+                    'Pensil dengan pena yang nyaman dan mudah digunakan serta harga terjangkau, cocok untuk menulis dan menggambar',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey.shade700,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+
+                  Center(
+                    child: ElevatedButton.icon(
+                      onPressed: (){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Produk ditambahkan ke keranjang')),
+                        );
+                      }, 
+                      icon: Icon(Icons.add_shopping_cart_rounded),
+                      label: Text('Tambah ke keranjang')),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          
-        }, 
-        child: Text('Add to cart'),
       ),
     );
   }
