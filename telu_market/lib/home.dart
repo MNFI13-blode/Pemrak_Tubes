b72e9b5ab0d0a8ab84ed97c6c829fe1d8e9f3ebf
MNFI13-blode <naufaltaurus24@gmail.com>
2025-01-02 21:21:54 +0700

    update homescreen

--- telu_market/lib/home.dart
+++ telu_market/lib/home.dart
@@ -1,10 +1,8 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
-import 'package:telu_market/deskripsi_merchant.dart';
-import 'package:telu_market/deskripsi_produk.dart';
 import 'package:telu_market/receipt.dart';
 import 'profile_page.dart';
-import 'cart_page.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,218 +10,107 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<String> categories = ["All", "Pensil", "Pena"];
+  String selectedCategory = "All"; // Gunakan string untuk kategori yang dipilih
   int currentIndex = 0;
-  final PageController pageController = PageController();
-  final List<Map<String, String>> items = [
-    {'title': 'HQ Mechanic Pencil 2B', 'price': 'Rp. 18.000'},
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(100), // Tinggi custom untuk AppBar
-        child: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          automaticallyImplyLeading: false, // Tidak menampilkan tombol back
-          flexibleSpace: Padding(
-            padding: const EdgeInsets.only(
-                top: 10, left: 16), // Padding atas dan kiri
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Welcome Back,',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                Text(
-                  'User',
-                  style: TextStyle(
-                    fontSize: 32, // Ukuran teks untuk User
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-          ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: null,
+        title: Text(
+          "Welcome, User",
+          style: TextStyle(color: Colors.black, fontSize: 25),
         ),
+        // brightness: Brightness.light,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Iconsax.shopping_cart,
+              color: Colors.black,
+            ),
+          )
+        ],
       ),
-
-      body: PageView(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 10),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Search in thousands of products',
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
-                      ),
-                      fillColor: Colors.grey[200],
-                      filled: true,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Merchant Store',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 10),
-                  ItemCard(
-                    title: 'Toko Sukabirus',
-                    originalPrice: '',
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => DeskripsiMerchant()));
-                    },
-                  ),
-                  Divider(),
-                  ItemCard(
-                    title: 'Toko Sukapura',
-                    originalPrice: '',
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => DeskripsiMerchant())
-                      );
-                    },
-                  ),
-                  Divider(),
-                  ItemCard(
-                    title: 'Toko Global',
-                    originalPrice: '',
-                    onTap: (){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => DeskripsiMerchant())
-                      );
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Item Card',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.add_circle, color: Colors.orange),
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              String? itemName;
-                              String? itemPrice;
-
-                              return AlertDialog(
-                                title: Text('Add Item'),
-                                content: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    TextField(
-                                      decoration: InputDecoration(
-                                          hintText: 'Item Name'),
-                                      onChanged: (value) {
-                                        itemName = value;
-                                      },
-                                    ),
-                                    TextField(
-                                      decoration: InputDecoration(
-                                          hintText: 'Item Price'),
-                                      keyboardType: TextInputType.number,
-                                      onChanged: (value) {
-                                        itemPrice = value;
-                                      },
-                                    ),
-                                  ],
-                                ),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text('Cancel'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      if (itemName != null &&
-                                          itemPrice != null) {
-                                        setState(() {
-                                          items.add({
-                                            'title': itemName!,
-                                            'price': itemPrice!
-                                          });
-                                        });
-                                      }
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text('Add'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: items.length,
-                      itemBuilder: (context, index) {
-                        final item = items[index];
-                        return Column(
-                          children: [
-                            ItemCard(
-                              title: item['title']!,
-                              originalPrice: item['price']!,
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            DeskripsiProduk()));
-                              },
-                            ),
-                            Divider(),
-                          ],
-                        );
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+               // Ganti bagian ListView biasa dengan ListView.builder
+              Container(
+                height: 40,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedCategory = categories[index]; // Update kategori yang dipilih
+                        });
                       },
+                      child: AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        margin: EdgeInsets.only(right: 10),
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                          color: selectedCategory == categories[index]
+                              ? Colors.orange // Warna saat kategori dipilih
+                              : Colors.grey[200],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Center(
+                          child: Text(
+                            categories[index],
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: selectedCategory == categories[index]
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.spaceBetween, // Menjaga jarak antar card
+                children: [
+                  Expanded(
+                    child: FadeInUp(
+                      duration: Duration(milliseconds: 1500),
+                      child: makeItem(
+                        image: "lib/assets/login.png",
+                        context: context,
+                      ),
                     ),
                   ),
-                  // ItemCard(
-                  //   title: 'HQ Mechanic Pencil 2B',
-                  //   originalPrice: 'Rp. 18.000',
-                  //   onTap: () {
-                  //     Navigator.push(
-                  //         context,
-                  //         MaterialPageRoute(
-                  //             builder: (context) => DeskripsiProduk()));
-                  //   },
-                  // ),
+                  SizedBox(width: 10), // Memberikan jarak antar card
+                  Expanded(
+                    child: FadeInUp(
+                      duration: Duration(milliseconds: 1500),
+                      child: makeItem(
+                        image: "lib/assets/login.png",
+                        context: context,
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
@@ -247,7 +134,7 @@ class _HomeScreenState extends State<HomeScreen> {
               print("Navigasi ke Order");
               break;
             case 2:
-              Navigator.push(context, 
+              Navigator.push(context,
                   MaterialPageRoute(builder: (context) => ReceiptScreen()));
               break;
             case 3:
@@ -256,7 +143,7 @@ class _HomeScreenState extends State<HomeScreen> {
               break;
           }
         },
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Iconsax.home),
             label: 'Home',
@@ -275,65 +162,113 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () {
-      //     Navigator.push(
-      //       context,
-      //       MaterialPageRoute(builder: (context) => CartPage()),
-      //     );
-      //   },
-      //   child: Icon(Icons.shopping_cart),
-      //   backgroundColor: Colors.red,
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
-}
 
-class ItemCard extends StatelessWidget {
-  final String title;
-  final String originalPrice;
-  final String? description;
-  final VoidCallback? onTap; // Callback untuk aksi ketika ditekan
-
-  ItemCard({
-    required this.title,
-    required this.originalPrice,
-    this.description,
-    this.onTap, // Tambahkan parameter onTap
-  });
-
-  @override
-  Widget build(BuildContext context) {
+  Widget makeItem({required String image, BuildContext? context}) {
     return GestureDetector(
-      onTap: onTap, // Tangkap aksi ketika widget ditekan
       child: Container(
-        padding: EdgeInsets.all(16),
+        width: 220,
+        height: 200,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.only(bottom: 20),
+        alignment: Alignment.centerLeft,
         decoration: BoxDecoration(
-          color: Colors.red[100],
-          borderRadius: BorderRadius.circular(10),
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.red,
+          image: DecorationImage(
+            image: AssetImage(image),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+              Colors.red.withOpacity(0.3),
+              BlendMode.darken,
+            ),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade400,
+              blurRadius: 10,
+              offset: const Offset(0, 10),
+            )
+          ],
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-            SizedBox(height: 5),
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
             Row(
-              children: [
-                Text(
-                  originalPrice,
-                  style: TextStyle(
-                    color: Colors.black,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      FadeInUp(
+                        duration: const Duration(milliseconds: 1000),
+                        child: const Text(
+                          "Sneakers",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20, // Font lebih kecil
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      FadeInUp(
+                        duration: const Duration(milliseconds: 1100),
+                        child: const Text(
+                          "Nike",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(width: 10),
               ],
             ),
-            if (description != null)
-              Text(
-                description!,
-                style: TextStyle(color: Colors.red[800], fontSize: 12),
+            FadeInUp(
+              duration: const Duration(milliseconds: 1200),
+              child: const Text(
+                "100\$",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
+            ),
+            // Menambahkan tombol "Tambah ke Keranjang"
+            FadeInUp(
+                duration: const Duration(milliseconds: 1300),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Aksi untuk menambahkan ke keranjang
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange, // Warna tombol
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15), // Menambah padding horizontal
+                  ),
+                  child: Text(
+                    "Tambahkan",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize:
+                          14, // Mengatur ukuran font lebih kecil agar pas dalam tombol
+                    ),
+                    textAlign: TextAlign.center, // Menjaga teks tetap di tengah
+                    overflow: TextOverflow
+                        .ellipsis, // Menghindari teks keluar dari tombol jika terlalu panjang
+                  ),
+                )),
           ],
         ),
       ),
